014e1ad0e00c68ec783c04c80c9317f4ca09d809
Swayam <167863217+tiwarienum@users.noreply.github.com>
2024-04-24 16:20:16 +0530

    Bsolution

---
+++ Arrays/BSolution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BSolution {
+    public static void Reverse(long[] arr, int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print(arr[n - 1]+" ");
+        Reverse(arr, n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n < 1 || n > 100) {
+            return;
+        }
+        long[] arr = new long[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextLong();
+            if (arr[i] < 0 || arr[i] > Math.pow(10, 18))
+                return;
+        }
+       Main.Reverse(arr,n);
+    }
+}
